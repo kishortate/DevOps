8f7f7b81f4257a0e9001a31eb5ce98283ecec815
kishortate <tatekishor54@gmail.com>
2024-10-08 14:01:54 +0000

    for_loop in shell

---
+++ for_loop.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#Script for for loop
+
+#method 1
+for i in 1 2 3 4 5
+do
+echo "Number is $i"
+done
+echo "------------------------------------------"
+#Method 2
+for j in {1..5}
+do 
+	echo "Number is $j"
+done	
+
+echo "-----------------------------------------------"
+
+#Method 3
+
+for name in Raj Ravi Ram
+do
+	echo "$name"
+done
+echo "---------------------------------------------"
+
+#conditional statements examples
+
+#1) compairing two numbers
+a=10
+b=20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
