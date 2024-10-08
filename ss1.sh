0a95b3f2d40071f7f64f6c924c630687b90dbc00
kishortate <tatekishor54@gmail.com>
2024-10-08 10:12:10 +0000

    My first script

---
+++ ss1.sh
@@ -0,0 +1,12 @@
+echo "Hello World !"
+
+echo "Today's date and time is $(date)"
+
+read -p "Enter no 1: " num1
+
+read -p "Enter no 2 :" num2
+
+
+echo "The Addition of $num1 and $num2 is $((num1+num2))"
+
+
