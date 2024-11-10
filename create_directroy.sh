abe46d34a659c29dc044ab088992f1c3fc41a35a
kishortate <ubuntu@ip-172-31-42-42.ec2.internal>
2024-11-10 14:28:08 +0000

    added modified script

--- create_directroy.sh
+++ create_directroy.sh
@@ -8,10 +8,17 @@ read -p "Enter end number to create directories: " end_num
 echo "Directories with prefix $dir_name from $start_num to $end_num will be created"
 
 function create_dir {
-for (( i=$start_num;i<=$end_num;i++ ))
+for (( i=start_num;i<=end_num;i++ ))
 do
 	create_directory=$(mkdir "${dir_name}${i}")
 done
 }
 create_dir
-echo "Directory creation process completed"
+
+if [ $? -ne 0 ]
+then 
+	echo "Failed to create directory"
+else
+	echo "Directory creation process completed"
+fi
+
